74a0d20210c38d9ee65afe3dbebccc3baac8238d
mdashik-dev <mdashik8495@gmail.com>
2025-07-24 11:39:37 +0600

    forgot password and change password feature added

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,16 +6,28 @@ interface EnvConfig {
     PORT: string,
     DB_URL: string,
     NODE_ENV: "development" | "production",
+    FRONTEND_URL: string, // Optional for frontend URL
     BCRYPT_SALT_ROUNDS: string,
     JWT_ACCESS_SECRET: string,
     JWT_ACCESS_EXPIRES: string,
     JWT_REFRESH_SECRET: string,
     JWT_REFRESH_EXPIRES: string,
-    EXPRESS_SESSION_SECRET: string
+    EXPRESS_SESSION_SECRET: string,
+    EMAIL_SENDER: {
+        SMTP_USER: string;
+        SMTP_PASS: string;
+        SMTP_PORT: string;
+        SMTP_HOST: string;
+        SMTP_FROM: string;
+    };
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUNDS", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "EXPRESS_SESSION_SECRET"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "FRONTEND_URL", "NODE_ENV", "BCRYPT_SALT_ROUNDS", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "EXPRESS_SESSION_SECRET", "SMTP_PASS",
+        "SMTP_PORT",
+        "SMTP_HOST",
+        "SMTP_USER",
+        "SMTP_FROM",];
 
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
@@ -26,13 +38,21 @@ const loadEnvVariables = (): EnvConfig => {
     return {
         PORT: process.env.PORT as string,
         DB_URL: process.env.DB_URL!,
+        FRONTEND_URL: process.env.FRONTEND_URL as string,
         NODE_ENV: process.env.NODE_ENV as "development" | "production",
         BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS || "10",
         JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
         JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES || "1h",
         JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
         JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES || "30d",
-        EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string
+        EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+        EMAIL_SENDER: {
+            SMTP_USER: process.env.SMTP_USER as string,
+            SMTP_PASS: process.env.SMTP_PASS as string,
+            SMTP_PORT: process.env.SMTP_PORT as string,
+            SMTP_HOST: process.env.SMTP_HOST as string,
+            SMTP_FROM: process.env.SMTP_FROM as string,
+        },
     }
 }
 
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -6,8 +6,11 @@ import { setAuthCookie } from "../../utils/setAuthCookie";
 import { sendResponse } from "../../utils/sendResponse";
 
 import httpStatus from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
+import { AuthServices } from "./auth.service";
+import { JwtPayload } from "jsonwebtoken";
 
-const credentialsLogin = async (req: Request, res: Response, next: NextFunction) => {
+const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     passport.authenticate("local", async (err: any, user: any, info: any) => {
 
         if (err) {
@@ -35,8 +38,52 @@ const credentialsLogin = async (req: Request, res: Response, next: NextFunction)
             },
         })
     })(req, res, next)
-}
+})
+
+const forgotPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const { email } = req.body;
+    await AuthServices.forgotPassword(email);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Email Sent Successfully",
+        data: null,
+    })
+})
+
+const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const decodedToken = req.user
+
+    await AuthServices.resetPassword(req.body, decodedToken as JwtPayload);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Password Changed Successfully",
+        data: null,
+    })
+})
+
+const changePassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user
+
+    await AuthServices.changePassword(oldPassword, newPassword, decodedToken as JwtPayload);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Password Changed Successfully",
+        data: null,
+    })
+})
 
 export const AuthControllers = {
-    credentialsLogin
+    credentialsLogin,
+    forgotPassword,
+    resetPassword,
+    changePassword
 }
